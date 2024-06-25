a06c23aa7db97d03c7bbfce0d8ec0cb96467b9df
Ion <teloslinux@protonmail.com>
2024-06-26 00:14:31 +0300

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  // Handle notification click event (e.g., open a specific URL)
+});
+
+self.addEventListener('notificationclose', event => {
+  console.log('Notification closed:', event);
+});
+
+self.addEventListener('push', event => {
+  console.log('Push notification received:', event);
+  const notificationOptions = {
+    body: 'This is a push notification from your web app!',
+    icon: 'https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsoutlined/mail/default/48px.svg''
+  };
+  event.waitUntil(self.registration.showNotification('Push Notification', notificationOptions));
+});
+ 
