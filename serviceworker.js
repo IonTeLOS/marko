95e9149ff37ca80cea900f5105c6ae99dc2bdc1e
Ion <teloslinux@protonmail.com>
2024-06-26 23:11:11 +0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -7,18 +7,16 @@ self.addEventListener('activate', event => {
   console.log('Service Worker activated.');
 });
 
-// Example: Listen for messages from the main thread
 self.addEventListener('message', event => {
   if (event.data && event.data.action === 'scheduleNotification') {
-    const { title, body, icon, timeToShow } = event.data.payload;
-
-    // Schedule the notification to show at the specified time
+    const notificationData = event.data.notification;
+    
     setTimeout(() => {
-      self.registration.showNotification(title, {
-        body: body,
-        icon: icon
+      self.registration.showNotification(notificationData.title, {
+        body: notificationData.body,
+        icon: notificationData.icon
       });
-    }, timeToShow - Date.now()); // Calculate the delay
+    }, notificationData.delay);
   }
 });
 
