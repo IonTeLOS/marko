46413f1c16612cf8f528db742331b1f99336e461
Ion <teloslinux@protonmail.com>
2024-06-26 23:15:57 +0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -7,6 +7,35 @@ self.addEventListener('activate', event => {
   console.log('Service Worker activated.');
 });
 
+self.addEventListener('push', event => {
+  console.log('Push notification received:', event);
+
+  let notificationData = {
+    title: 'Default Title',
+    body: 'This is a default notification message.',
+    icon: 'https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsoutlined/mail/default/48px.svg'
+  };
+
+  if (event.data) {
+    try {
+      const payload = event.data.json();
+      notificationData = {
+        ...notificationData,
+        ...payload
+      };
+    } catch (e) {
+      console.error('Error parsing push notification data:', e);
+    }
+  }
+
+  event.waitUntil(
+    self.registration.showNotification(notificationData.title, {
+      body: notificationData.body,
+      icon: notificationData.icon
+    })
+  );
+});
+
 self.addEventListener('message', event => {
   if (event.data && event.data.action === 'scheduleNotification') {
     const notificationData = event.data.notification;
@@ -16,7 +45,7 @@ self.addEventListener('message', event => {
         body: notificationData.body,
         icon: notificationData.icon
       });
-    }, notificationData.delay);
+    }, notificationData.delay || 0);
   }
 });
 
