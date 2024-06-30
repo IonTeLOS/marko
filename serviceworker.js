2fb576a6f85ec3394880805cb6f6c7a6cb6c252d
Ion <teloslinux@protonmail.com>
2024-06-30 09:19:23 +0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -91,4 +91,21 @@ self.addEventListener('notificationclick', event => {
     event.waitUntil(clients.openWindow(defaultUrl));
   }
 });
+self.addEventListener('message', event => {
+    if (event.data && event.data.action === 'cancelNotification') {
+        const uuid = event.data.uuid;
+        console.log(`Cancel notification request received for UUID: ${uuid}`);
+
+        // Get all notifications and find the one with the matching UUID
+        self.registration.getNotifications().then(notifications => {
+            notifications.forEach(notification => {
+                const notificationUUID = notification.data.uuid;
+                if (notificationUUID === uuid) {
+                    notification.close();
+                    console.log(`Canceled notification with UUID ${uuid}`);
+                }
+            });
+        });
+    }
+});
 
