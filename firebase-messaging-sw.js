b7a810a075fd7142dd5c13a474ca8b93945ddd11
Ion <teloslinux@protonmail.com>
2024-07-02 22:01:51 +0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,42 @@
+importScripts('https://www.gstatic.com/firebasejs/8.6.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.2/firebase-messaging.js');
+
+// Firebase configuration object
+    const firebaseConfig = {
+    apiKey: "AIzaSyD96IBVqGKVEdmXIVCYL_7kvlBhJNSD1Ww",
+    authDomain: "marko-be9a9.firebaseapp.com",
+    projectId: "marko-be9a9",
+    storageBucket: "marko-be9a9.appspot.com",
+    messagingSenderId: "7036670175",
+    appId: "1:7036670175:web:99992356716578ea13996a"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase Messaging
+var messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
+
+  // Customize notification here
+  var notificationTitle = payload.notification.title;
+  var notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+    data: {
+      url: `https://teloslinux.org/marko/newfile?uuid=${payload.data.uuid}`
+    }
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});
+ 
